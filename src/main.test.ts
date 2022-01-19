7e9a56552c23bf2b4a8271dd7041cafe1b4cdf97
Juan Sulca <juan.sulca@devsu.com>
2022-01-18 21:09:40 -0500

    call PicoPlaca class from main and format output

--- src/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { someFunc } from './main';
-
-describe('some Func', () => {
-  test('should return 2', () => {
-    expect(someFunc()).toEqual(2);
-  });
-});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-export const someFunc = () => {
-  return 2;
-};
+import { PicoPlaca } from './PicoPlaca';
 
-console.log('done!');
+const input = [
+  {
+    plate: 'PCP-2613',
+    date: '2022-01-18',
+    time: '23:42',
+  },
+  {
+    plate: 'PCP-2613',
+    date: '2022-01-18',
+    time: '18:42',
+  }
+];
+
+const picoPlaca = new PicoPlaca();
+
+for (const data of input) {
+  const output = picoPlaca.canCirculate(data);
+  console.log(`🚗: ${data.plate} ${output ? 'can' : 'can not'} circulate ${data.date} @ ${data.time} ${output ? '✅' : '❌'}`);
+}
+
+console.log('Done!');
